b11fc14140c1ab7e323244e88b8cbf65c29b9fc8
vaporvare <133262832+vaporvare@users.noreply.github.com>
2023-12-04 19:03:05 +0500

    audio nu control karta maiiiii

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AudioSettings : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Slider volumeSlider;
+
     void Start()
     {
-        
-    }
 
+        volumeSlider.value = AudioListener.volume;
+
+    }
+    public void onVolumeSliderChanged()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnBackButton()
