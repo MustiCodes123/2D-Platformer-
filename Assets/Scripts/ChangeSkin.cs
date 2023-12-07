9b5b4e89e5527db59960df23ad9f2dc0ffdbe335
Rafay Junaid <rafooaj11@gmail.com>
2023-12-07 20:04:31 +0500

    Added skin icon in pause menu

--- Assets/Scripts/ChangeSkin.cs
+++ Assets/Scripts/ChangeSkin.cs
@@ -1,12 +1,20 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class ChangeSkin : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] RuntimeAnimatorController animatorController;
-    [SerializeField] AnimatorOverrideController animatorOverrideController;
+    [SerializeField] AnimatorOverrideController[] extraSkins;
+    public UnityEngine.UI.Image img;
+    public Sprite[] allSkins;
+
+    int index = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +25,19 @@ public class ChangeSkin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        img.sprite = allSkins[index];
     }
     public void UpdateSkin()
     {
-        if (animator.runtimeAnimatorController == animatorController)
+        if (index == extraSkins.Length)
         {
-            animator.runtimeAnimatorController = animatorOverrideController;
+            animator.runtimeAnimatorController = animatorController;
+            index = 0;
         }
         else
-            animator.runtimeAnimatorController = animatorController;
+        {
+            animator.runtimeAnimatorController = extraSkins[index];
+            index++;
+        }
     }
 }
